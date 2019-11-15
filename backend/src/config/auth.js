3e6c4a3c27074a705e98a072c2ed800a1a04017c
Agnaldo Burgo Junior <agnaldoburgojr@gmail.com>
2019-11-15 16:14:36 -0300

    add sequelize, authentication and student

---
+++ backend/src/config/auth.js
@@ -0,0 +1,4 @@
+export default {
+  secret: '9c5ff9d9314138ac3a4a13614bdab5de',
+  expiresIn: '7d',
+};
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,9 +1,16 @@
 import { Router } from 'express';
 
+import SessionController from './app/controllers/SessionController';
+import StudentsController from './app/controllers/StudentsController';
+
+import authMiddleware from './app/middlewares/auth';
+
 const routes = new Router();
 
-routes.get('/', (req, res) => {
-  return res.json({ ok: 'hello' });
-});
+routes.post('/sessions', SessionController.store);
+
+routes.use(authMiddleware);
+routes.post('/students', StudentsController.store);
+routes.put('/students/:id', StudentsController.update);
 
 export default routes;
